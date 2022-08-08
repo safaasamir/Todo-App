0b768e890c34f22ae982049c5c5ef1785c363e9c
safaasamirsherif <safaasamirsherif@gmail.com>
2022-08-09 00:24:26 +0200

    Route3+Style

--- src/component/meetup/MeetupItem.js
+++ src/component/meetup/MeetupItem.js
@@ -1,22 +1,31 @@
-//import style from"./MeetupItem.module.css"
+import style from"./MeetupItem.module.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
  import { library } from "@fortawesome/fontawesome-svg-core";
  import {  faStar } from '@fortawesome/free-solid-svg-icons'
+//  import MeeetupList from"./MeeetupList"
 
  library.add(faStar);
+
 function MeetupItem(props)
 {
-    const{image,title,discription}=props;
+    const{img,title,description}=props;
+  
 
-    <div className="card">
-
-    <img className="card-img-top" src={image} alt={title}/>
-    <div className="card-body">
-      <h5 className="card-title">{title}</h5>
-      <p className="card-text">{discription}</p>
-      <abutton className="btn btn-primary"><FontAwesomeIcon icon="star" />Name</abutton>
-    </div>
-  </div>
- 
+  return(
+    <div className={style.big}>
+    <div className={style.item }>
+          <div className= {style.image}>
+            <img className={style.img} src={img} alt={title}/>
+            </div>
+                 <div className={style.content}>
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{description}</p>
+                    <div>
+                      <button className="btn btn-dark"><FontAwesomeIcon icon="star" /></button>
+                    </div>
+                 </div>
+      </div>
+      </div>
+  )
 }
 export default MeetupItem
