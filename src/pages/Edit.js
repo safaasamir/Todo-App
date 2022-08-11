5c3b1e18a650e7c98bc328f8f71b8524fb7bbf6e
safaasamirsherif <safaasamirsherif@gmail.com>
2022-08-11 03:07:24 +0200

    delete_data

---
+++ src/pages/Edit.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import EditForm from '../component/meetup/EditForm'
+
+function Edit() {
+
+
+  
+  return (
+    <div><h3 className='m-3'> Edit Data</h3><EditForm/></div>
+  )
+}
+
+export default Edit
